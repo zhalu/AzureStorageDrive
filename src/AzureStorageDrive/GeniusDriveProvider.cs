f874f7ec84723610461dc06cd77a1a35eb1e0de6
leoyeah <leochou.xm@gmail.com>
2014-07-30 21:26:44 +0800

    Fix remove directory object in Azure blob

--- src/AzureStorageDrive/GeniusDriveProvider.cs
+++ src/AzureStorageDrive/GeniusDriveProvider.cs
@@ -216,6 +216,7 @@ namespace AzureStorageDrive
 
             if (sourceIsLocal && targetIsLocal)
             {
+                //in theory, supported. But users have more efficient ways to achieve this, so block this behavior.
                 throw new NotSupportedException();
             }
 
